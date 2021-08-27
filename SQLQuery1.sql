d03ccff043fce289c28e746fad7c2b249c7a53c3
akhil2502 <akhilachilies>
2021-08-27 17:33:17 +0530

    [akhil] Add:welcome

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -10,7 +10,7 @@ insert  into  employee_payroll values (1,'akhil','70000','26-9-2020')
 insert  into  employee_payroll values (2,'ram','80000','26-9-2020')
 insert  into  employee_payroll values (3,'akhil','80000','26-9-2020')
 insert  into  employee_payroll values (4,'sai','70000','26-9-2020')
-
+insert  into  employee_payroll values (5,'kajal','60000','26-9-2020','f')
 select * from employee_payroll
 
 select * from employee_payroll where name = 'akhil'
@@ -22,8 +22,34 @@ update  employee_payroll
 set salary = 90000
 where num = 1
 
+select salary from employee_payroll where num=2
+select salary from employee_payroll where name='sai'
 
+select * from employee_payroll where name = 'sai' and startdate between cast('26-9-2020' as date) and getdate()
 
+alter table employee_payroll
+add gender  varchar(1) 
+update  employee_payroll
+set gender = 'm'
+where name='akhil' or name='ram'or name='sai'
 
+alter table employee_payroll
+alter column salary int
+go
 
+select sum(salary), gender from employee_payroll
+group by gender
 
+select avg(salary),gender from employee_payroll
+group by gender
+
+select sum(salary) from employee_payroll
+
+select min(salary),gender from employee_payroll
+group by gender
+
+select max(salary),gender from employee_payroll
+group by gender
+
+select count(salary),gender from employee_payroll
+group by gender
